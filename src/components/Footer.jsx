e897d2c584cb30bc96f34ff9fb3d73cc05ca3c73
ShaikAleemAhamed123 <shaikaleemahamed786@gmail.com>
2022-03-07 07:53:23 +0530

    Added Contact Mail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer(){
         <a target="_blank" href="https://www.instagram.com/aleemahamedshaik/"><i className=" instagram fab1 fab fa-instagram fa-2x mx-3"></i></a>
         <a target="_blank" href="https://github.com/ShaikAleemAhamed123"> <i className="github fab2 fab fa-github  fa-2x mx-3"></i></a>
         <a target="_blank" href="https://www.linkedin.com/in/aleem-ahamed-shaik-81123a1ab/"><i className="linkedin fab3 fab fa-linkedin fa-2x mx-3"></i></a>
-        <a action="mailto:shaikaleemahamed786@gmail.com"><i className="email far fa-envelope fa-2x mx-3"></i></a>
+        <a href="mailto:shaikaleemahamed786@gmail.com"><i className="email far fa-envelope fa-2x mx-3"></i></a>
          
     </div>
 
