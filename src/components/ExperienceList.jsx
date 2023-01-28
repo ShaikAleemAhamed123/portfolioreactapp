5d5bd4f293427c2d21f49b372ca64e63991fdd2d
Aleem Ahamed <shaikaleemahamed786@gmail.com>
2023-01-28 13:21:02 +0530

    Added TCP Mentorship to experiences List

--- src/components/ExperienceList.jsx
+++ src/components/ExperienceList.jsx
@@ -1,8 +1,15 @@
 
 const ExperienceList = [
     {
+        title: "Mentor at TCP Mentorship Program, NIT Raipur.",
+        time: "(Nov 2022-Present)",
+        role: "DSA Mentor.",
+        description1: "Responsible for guiding the students in the field of DSA and Competitive Programming.",
+        description2: "TCP (Turing club of Programmers) is a club that aims to inculcate the skills of DSA and Competitive Programming in students and cultivate coding culture in college."
+    },
+    {
         title: "ACE Committee Member, NIT Raipur.",
-        time: "(Oct 2021-Present)",
+        time: "(Oct 2021-Nov-2022)",
         role: "Web Dev Team Member.",
         description1: "Member of the team developing and maintaing a website for the Committee.",
         description2: "Responsible for holding various technical events for students in the college to cultivate industrial skills and exposure."
