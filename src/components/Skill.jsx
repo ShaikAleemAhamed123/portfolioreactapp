0036962fc46141ed3c7c83bc207a7a2c374030d3
ShaikAleemAhamed123 <shaikaleemahamed786@gmail.com>
2024-06-10 21:48:02 +0530

    style changes

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 function Skill(props) {
-    return <div className="skill col col-12 col-md-6 col-lg-4 md-3 ">
+    return <div className="skill col col-12 col-md-6 col-lg-6 col-xl-4 md-3 ">
         <div className="d-flex flex-row align-items-center justify-content-between py-3">
             <p className="skill-heading">{props.title}</p>
             <img className="rounded-circle z-depth-2" style={{ maxHeight: "90px", maxWidth: "90px" }} src={props.image} alt="Loading" />
